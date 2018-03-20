dd93b9f98ef646fd1eb11f96ad817814805949ac
exame <exame@tjmt.jus.br>
2018-03-20 15:18:46 -0400

    Criação de classe responsável pela comunicação com o banco de dados

---
+++ ChocolateSystem/ChocolateSystem/App_Code/BancoDeDados.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+public class BancoDeDados
+{
+    private SqlConnection _sqlConection;
+
+    public BancoDeDados()
+    {
+        string stringDeConexao = "server='DESKTOP-5QQDCV3\\MSSQLSERVEREXAME'; database='Chocolate'; user='sa'; password=123456; integrated security='false' ";
+        _sqlConection = new SqlConnection(stringDeConexao);
+    }
+
+    public DataTable BuscaMassaDeDados(string query) {
+        var sqlCommand = new SqlCommand(query, _sqlConection);
+        var resultado = new DataTable();
+        resultado.Load(sqlCommand.ExecuteReader());
+        return resultado;
+    }
+}
